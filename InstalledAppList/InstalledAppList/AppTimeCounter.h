da1b9f3fbd36e967d13b8a12c8c3067e459c2dfe
gongliang <gongliang@aprbrother.com>
2015-01-30 14:14:31 +0800

    delete

--- InstalledAppList/InstalledAppList/AppTimeCounter.h
+++ InstalledAppList/InstalledAppList/AppTimeCounter.h
@@ -2,10 +2,8 @@
 //  AppTimeCounter.h
 //  AppTime
 //
-//  Created by gugupluto on 14-2-23.
-//  Copyright (c) 2013年 gugupluto. All rights reserved.
-//  http://www.cnblogs.com/gugupluto
-//  https://github.com/gugupluto/iOSAppTime
+
+
 
 #import <Foundation/Foundation.h>
 @interface AppTimeCounter : NSObject
--- InstalledAppList/InstalledAppList/AppTimeCounter.m
+++ InstalledAppList/InstalledAppList/AppTimeCounter.m
@@ -1,11 +1,7 @@
 //
 //  AppTimeCounter.m
 //  AppTime
-//
-//  Created by gugupluto on 14-2-23.
-//  Copyright (c) 2013年 gugupluto. All rights reserved.
-//  http://www.cnblogs.com/gugupluto
-//  https://github.com/gugupluto/iOSAppTime
+
 
 #import "AppTimeCounter.h"
 #import  <objc/runtime.h>
